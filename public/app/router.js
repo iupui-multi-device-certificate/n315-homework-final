61c3940afc9566bc737f7d4ce354f8b876585047
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-04-02 13:22:39 -0400

    feature: basic views

--- public/app/router.js
+++ public/app/router.js
@@ -1,11 +1,11 @@
 console.log("router loaded");
 
 const routes = {
-  home: "<h1>home</h1>",
-  browse: "<h1>browse</h1>",
-  createRecipe: "createRecipe",
-  yourRecipes: "yourRecipes",
-  login: "login",
+  home: homeView(),
+  browse: browseView(),
+  createRecipe: createRecipeView(),
+  yourRecipes: yourRecipesView(),
+  login: loginView(),
 };
 
 function locationHashChanged() {
@@ -21,6 +21,26 @@ function locationHashChanged() {
 
   toggleCurrentPage(hashTag);
 }
+
+const toggleCurrentPage = (currentPageHash) => {
+  //https://codepen.io/Coding-in-Public/pen/MWroExJ
+  //except also need to remove from rest when change page
+  //get their hash not href (which is the whole link)
+
+  document.querySelectorAll(".nav-menu .nav-link").forEach((navLink) => {
+    //just get the anchor tags
+    if (navLink.tagName === "A") {
+      //use aria-current for accessibility reasons
+
+      //remove aria-current from everywhere first
+      navLink.removeAttribute("aria-current", "page");
+
+      if (navLink.hash === currentPageHash && navLink.hash != "#login") {
+        navLink.setAttribute("aria-current", "page");
+      }
+    }
+  });
+};
 
 // const changeRoute = () => {
 //   let hashTag = window.location.hash;
---
+++ public/app/views.js
@@ -0,0 +1,9 @@
+const homeView = () => `<h1>home</h1>`;
+
+const browseView = () => `<h1>browse</h1>`;
+
+const createRecipeView = () => `createRecipe`;
+
+const yourRecipesView = () => `yourRecipes`;
+
+const loginView = () => `login`;
